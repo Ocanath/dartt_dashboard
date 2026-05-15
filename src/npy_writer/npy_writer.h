a6a099cf0247484a870e28a347c88255d8962ef2
ocanath <ocanath@gmail.com>
2026-05-15 15:31:44 -0700

    start numpy tu

---
+++ src/npy_writer/npy_writer.h
@@ -0,0 +1,26 @@
+#include <fstream>
+#include <string>
+
+class NpyWriter
+{
+	public:
+		enum type {
+			UINT8,
+			UINT16,
+			UINT32,
+			UINT64,
+			INT8,
+			INT16,
+			INT32,
+			INT64,
+			FLOAT32,
+			DOUBLE64
+		};
+		int open(std::string name);
+		int add_sample(void * data, size_t size, type type);
+		int close();
+	private:
+		uint64_t sample_count;
+		std::string _filename;
+};
+
