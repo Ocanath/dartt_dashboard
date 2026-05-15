8b6cb3ae4f06574c73ff8cb3f2eb1d45f63bf9c1
ocanath <ocanath@gmail.com>
2026-05-15 15:51:00 -0700

    type

--- src/npy_writer/npy_writer.h
+++ src/npy_writer/npy_writer.h
@@ -16,11 +16,12 @@ class NpyWriter
 			FLOAT32,
 			DOUBLE64
 		};
-		int open(std::string name);
+		int open(std::string name, type dtype);
 		int add_sample(void * data, size_t size, type type);
 		int close();
 	private:
 		uint64_t sample_count;
 		std::string _filename;
+		type _dtype;
 };
 
