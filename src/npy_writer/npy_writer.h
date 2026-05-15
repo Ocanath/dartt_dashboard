bdee37a26445cc1e448a674d0d697a421b899a0d
ocanath <ocanath@gmail.com>
2026-05-15 16:07:57 -0700

    vibed npy writer

--- src/npy_writer/npy_writer.h
+++ src/npy_writer/npy_writer.h
@@ -1,27 +1,35 @@
-#include <fstream>
+#pragma once
+#include <cstdio>
+#include <cstdint>
 #include <string>
+#include <atomic>
 
 class NpyWriter
 {
-	public:
-		enum type {
-			UINT8,
-			UINT16,
-			UINT32,
-			UINT64,
-			INT8,
-			INT16,
-			INT32,
-			INT64,
-			FLOAT32,
-			DOUBLE64
-		};
-		int open(std::string name, type dtype);
-		int add_sample(void * data, size_t size, type type);
-		int close();
-	private:
-		uint64_t sample_count;
-		std::string _filename;
-		type _dtype;
+public:
+    enum type {
+        UINT8,
+        UINT16,
+        UINT32,
+        UINT64,
+        INT8,
+        INT16,
+        INT32,
+        INT64,
+        FLOAT32,
+        DOUBLE64
+    };
+    int open(std::string name, type dtype);
+    int add_sample(void* data, size_t size, type dtype);
+    int close();
+    static int open_count();
+private:
+    FILE*       _file         = nullptr;
+    const char* _descr        = nullptr;
+    uint64_t    _sample_count = 0;
+    std::string _filename;
+    type        _dtype;
+
+    static std::atomic<int> _open_count;
 };
 
