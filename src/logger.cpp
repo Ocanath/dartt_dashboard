efa20c7c8948d93394c62b62ddc2fd9353315004
ocanath <ocanath@gmail.com>
2026-05-19 21:14:15 -0700

    reverted from round robin to greedy for reduced loop overhead and better file I/O performance. may need to just increase ring buffer size instead for better jitter absorption

--- src/logger.cpp
+++ src/logger.cpp
@@ -14,83 +14,74 @@ union LogVal {
     double   f64;
 };
 
-// Pop one item from the channel and write it. Returns true if an item was consumed.
-static bool drain_one(LogChannel* ch)
+static bool drain_ring_buffer(LogChannel* pLogChannel)
 {
-    LogVal val = {};
-    if (!ch->ring.pop(&val, sizeof(val)))
+    if (pLogChannel == nullptr)
         return false;
-    switch (ch->writer.get_dtype())
+
+    LogVal val = {};
+    NpyWriter::type dtype = pLogChannel->writer.get_dtype();
+    while (pLogChannel->ring.pop(&val, sizeof(val)))
     {
-        case NpyWriter::UINT8:
+        switch (dtype)
         {
-            ch->writer.add_uint8(val.u8);
-            break;
-        }
-        case NpyWriter::UINT16:
-        {
-            ch->writer.add_uint16(val.u16);
-            break;
-        }
-        case NpyWriter::UINT32:
-        {
-            ch->writer.add_uint32(val.u32);
-            break;
-        }
-        case NpyWriter::UINT64:
-        {
-            ch->writer.add_uint64(val.u64);
-            break;
-        }
-        case NpyWriter::INT8:
-        {
-            ch->writer.add_int8(val.i8);
-            break;
-        }
-        case NpyWriter::INT16:
-        {
-            ch->writer.add_int16(val.i16);
-            break;
-        }
-        case NpyWriter::INT32:
-        {
-            ch->writer.add_int32(val.i32);
-            break;
-        }
-        case NpyWriter::INT64:
-        {
-            ch->writer.add_int64(val.i64);
-            break;
-        }
-        case NpyWriter::FLOAT32:
-        {
-            ch->writer.add_float32(val.f32);
-            break;
-        }
-        case NpyWriter::DOUBLE64:
-        {
-            ch->writer.add_double64(val.f64);
-            break;
-        }
-        default:
-        {
-            break;
+            case NpyWriter::UINT8:
+            {
+                pLogChannel->writer.add_uint8(val.u8);
+                break;
+            }
+            case NpyWriter::UINT16:
+            {
+                pLogChannel->writer.add_uint16(val.u16);
+                break;
+            }
+            case NpyWriter::UINT32:
+            {
+                pLogChannel->writer.add_uint32(val.u32);
+                break;
+            }
+            case NpyWriter::UINT64:
+            {
+                pLogChannel->writer.add_uint64(val.u64);
+                break;
+            }
+            case NpyWriter::INT8:
+            {
+                pLogChannel->writer.add_int8(val.i8);
+                break;
+            }
+            case NpyWriter::INT16:
+            {
+                pLogChannel->writer.add_int16(val.i16);
+                break;
+            }
+            case NpyWriter::INT32:
+            {
+                pLogChannel->writer.add_int32(val.i32);
+                break;
+            }
+            case NpyWriter::INT64:
+            {
+                pLogChannel->writer.add_int64(val.i64);
+                break;
+            }
+            case NpyWriter::FLOAT32:
+            {
+                pLogChannel->writer.add_float32(val.f32);
+                break;
+            }
+            case NpyWriter::DOUBLE64:
+            {
+                pLogChannel->writer.add_double64(val.f64);
+                break;
+            }
+            default:
+            {
+                break;
+            }
         }
     }
     return true;
-}
-
-static void drain_all_fair(std::vector<std::unique_ptr<LogChannel>>& channels)
-{
-    bool any;
-    do 
-	{
-        any = false;
-        for (size_t i = 0; i < channels.size(); i++)
-		{
-            any |= drain_one(channels[i].get());
-		}
-    } while (any);
 }
 
 LoggerRingBuffer* DataLogger::add_channel(const std::string& filename,
@@ -98,11 +89,8 @@ LoggerRingBuffer* DataLogger::add_channel(const std::string& filename,
                                            size_t element_size)
 {
     std::unique_ptr<LogChannel> ch = std::make_unique<LogChannel>(element_size);
-    
-	if (ch->writer.open(filename, dtype) != 0)
-    {
-		return nullptr;
-	}
+    if (ch->writer.open(filename, dtype) != 0)
+        return nullptr;
     LoggerRingBuffer* ring = &ch->ring;
     std::lock_guard<std::mutex> lock(channels_mutex_);
     channels_.push_back(std::move(ch));
@@ -156,13 +144,15 @@ void DataLogger::file_writer_loop()
         if (channels_mutex_.try_lock())
         {
             std::lock_guard<std::mutex> lock(channels_mutex_, std::adopt_lock);
-            drain_all_fair(channels_);
+            for (size_t i = 0; i < channels_.size(); i++)
+                drain_ring_buffer(channels_[i].get());
         }
     }
     if (channels_mutex_.try_lock())
     {
         std::lock_guard<std::mutex> lock(channels_mutex_, std::adopt_lock);
-        drain_all_fair(channels_);
+        for (size_t i = 0; i < channels_.size(); i++)
+            drain_ring_buffer(channels_[i].get());
     }
     package();
 }
